fc8e86aca4272e3eff59b1adc8a72493e17c73e0
Sarise Kamanda de Oliveira <sarisekamanda@hotmail.com>
2018-12-02 09:58:42 -0200

    Initial commit

---
+++ app/src/main/java/com/example/sarise/mynotes/ListaActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sarise.mynotes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+
+
+    }
+}
